3bc2805a0c9b5a6235fa9b9b92cf97736fa665c4
Jobin Godson M S <jobingodsonms@gmail.com>
2026-07-26 17:31:37 +0530

    Update list comprehensions example

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,4 +1,4 @@
-#list=[i for i in range(20)]
+#list=[i for i in range(1,51)]
 #print(list) 
 
 #list=[i*i*i for i in range(10)]
@@ -14,3 +14,10 @@
 #list=[12,25,8,31,40]
 #pas=[ "pass" if lis>=20 else "fail" for lis in list]
 #print(pas)
+
+list=[i*i for i in range(1,51)]
+
+l2 = [i for i in list if i % 2 == 1]
+
+l3 = [str(i) for i in l2]
+print(l3)
