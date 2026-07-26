cd2ad9cbc218802868fd5df13c4e4709d9357e83
Jobin Godson M S <jobingodsonms@gmail.com>
2026-07-26 17:09:39 +0530

    Add list comprehensions file

---
+++ list_comprehensions.py
@@ -0,0 +1,16 @@
+#list=[i for i in range(20)]
+#print(list) 
+
+#list=[i*i*i for i in range(10)]
+#print(list)
+
+#list=[i for i in range(30) if i%2==1]
+#print(list)
+
+#list=["apple","banana","cherry"]
+#res=[lis.upper() for lis in list]
+#print(res)
+
+#list=[12,25,8,31,40]
+#pas=[ "pass" if lis>=20 else "fail" for lis in list]
+#print(pas)
