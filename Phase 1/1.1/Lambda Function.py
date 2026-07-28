df8e779a9684dbb89b12ddecbc6a3fb3967b08cd
Jobin Godson M S <jobingodsonms@gmail.com>
2026-07-28 22:57:51 +0530

    Add Lambda Function.py

---
+++ Phase 1/1.1/Lambda Function.py
@@ -0,0 +1,17 @@
+#pos = lambda a: a > 0
+#print(pos(5))  
+
+#maxi = lambda a,b: a if a > b else b
+#print(maxi(5,10))
+
+#res = lambda a: "pass" if a > 50 else "fail"
+#print(res(60))
+
+#year = lambda a: "leap year" if a % 4 == 0 and (a % 100 != 0 or a % 400 == 0) else "not leap year"
+#print(year(2020))
+
+#longest = lambda first, second, third: first if len(first) > len(second) and len(first) > len(third) else second if len(second) > len(third) else third#
+#print(longest("apple", "banana", "kiwi"))
+
+#grade= lambda marks: "A" if marks >= 90 else "B" if marks >= 80 else "C" if marks >= 70 else "D" if marks >= 60 else "F"
+#print(grade(85))
