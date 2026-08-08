30f236c2c533cb187ffd380d9b21f17a0fafa58b
Jobin Godson M S <jobingodsonms@gmail.com>
2026-08-08 23:29:56 +0530

    Add map/filter/reduce practice script

---
+++ Phase 1/1.1/map,filter,reduce.py
@@ -0,0 +1,35 @@
+#Original List
+  #    │
+ #     ▼
+ #map()     → Transform every item
+ #filter()  → Keep only selected items
+ #reduce()  → Combine all items into one result
+
+#________________________________________________________________
+
+#map() applies the same function to every element in an iterable.
+#map(function, iterable)
+#numbers = [1, 2, 3, 4, 5]
+#squares = list(map(lambda x: x**2, numbers))
+#print(squares)
+
+#________________________________________________________________
+
+#filter() removes items that don't satisfy a condition.
+#filter(function, iterable)
+#numbers = [1, 2, 3, 4, 5]
+#even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+#print(even_numbers)
+
+#________________________________________________________________
+
+#reduce() combines all elements into one final value.
+#reduce(function, iterable)
+#from functools import reduce
+#numbers = [2,3,4]
+#product = reduce(lambda a, b: a * b, numbers)
+#print(product)
+
+#________________________________________________________________
+#________________________________________________________________
+
