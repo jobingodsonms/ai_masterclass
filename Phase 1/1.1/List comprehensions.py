5b1b04c1f211fbb1b5d0f7280510f110fca5ce87
Jobin Godson M S <jobingodsonms@gmail.com>
2026-09-02 23:39:07 +0530

    Enhance List comprehensions.py with examples and exercises
    Added explanations and examples for list comprehensions in Python. Included mini practice exercises and an assignment for further learning.

--- Phase 1/1.1/List comprehensions.py
+++ Phase 1/1.1/List comprehensions.py
@@ -1,19 +1,48 @@
+A list comprehension is a shorter and more Pythonic way to create lists.
+Instead of writing multiple lines of code, you can write everything in one line.
+
+  
+NORMAL WAY:
+
+numbers = []
+for i in range(5):
+    numbers.append(i)
+print(numbers)
+
+LIST COMPREHENSION:
+syntax: new_list = [expression for item in iterable]
+
+
+Mini Practice:
+1. Create a list of numbers from 1 to 50
 #list=[i for i in range(1,51)]
 #print(list) 
 
+2.Create a list of cubes from 1 to 10.
 #list=[i*i*i for i in range(10)]
 #print(list)
 
+3.Create a list of only odd numbers from 1 to 30.
 #list=[i for i in range(30) if i%2==1]
 #print(list)
 
+4. Uppercase every word.
 #list=["apple","banana","cherry"]
 #res=[lis.upper() for lis in list]
 #print(res)
 
+5. if...else inside List Comprehension
 #list=[12,25,8,31,40]
 #pas=[ "pass" if lis>=20 else "fail" for lis in list]
 #print(pas)
+
+
+Assignment:
+
+1.Create a list of the first 50 squares.
+2.Remove all odd numbers using a list comprehension.
+3.Convert all remaining numbers into strings.
+4.Print the final list.
 
 list=[i*i for i in range(1,51)]
 
